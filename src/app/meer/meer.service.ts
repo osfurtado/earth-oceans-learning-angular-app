d4c190541ca4e9de3023d80c5be2c91ceced9115
Osvaldo Furtado <osvaldofurtado.bitlc@gmail.com>
2025-11-18 14:05:42 +0100

    Meer Service Created

--- src/app/meer/meer.service.ts
+++ src/app/meer/meer.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MeerDto } from './meer.dto';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MeerService {
+export class MeerService implements OnInit{
   
+  http = inject(HttpClient)
+  url: string = '/data/ozeans.json'
+
+
+  ngOnInit(): void {
+    
+  }
+
+  public getMeerById(id: number = 1): Observable<MeerDto> {
+
+    return this.http.get<MeerDto[]>(this.url).pipe(
+      map( data => {
+        return data.filter(m => m.id === id)[0]
+      })
+    )
+  }
+
+
 }
