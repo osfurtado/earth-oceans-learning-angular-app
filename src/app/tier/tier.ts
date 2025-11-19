29400d7cb976436ab6daaeb60a429f63351f850d
Osvaldo Furtado <osvaldofurtado.bitlc@gmail.com>
2025-11-19 15:42:20 +0100

    Working on Tier Page

--- src/app/tier/tier.ts
+++ src/app/tier/tier.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tier',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './tier.html',
   styleUrl: './tier.css',
 })
-export class Tier {
+export class Tier implements OnInit{
+
+  route = inject(ActivatedRoute)
+  router = inject(Router)
+  activeOcean!:string;
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( params => {
+      this.activeOcean = params.get('ozean') || ''
+      console.log('Este e o oceano ativo no Tiere Seite: ', this.activeOcean)
+    })
+    
+  }
+
+  onBack() {
+    this.router.navigate([`/${this.activeOcean}`])
+  }
 
 }
