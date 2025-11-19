41822cbe9355ecda8c5e132fcd7abf5e0559a770
Osvaldo Furtado <schurings@hotmail.com>
2025-11-19 22:21:46 +0100

    Tierpage Carussel created

--- src/app/tier/tier.ts
+++ src/app/tier/tier.ts
@@ -1,10 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
+
+interface TierDto {
+  id: number;
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-tier',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './tier.html',
   styleUrl: './tier.css',
 })
@@ -13,6 +20,8 @@ export class Tier implements OnInit{
   route = inject(ActivatedRoute)
   router = inject(Router)
   activeOcean!:string;
+  selectedItemId: number | null = null
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( params => {
@@ -25,5 +34,32 @@ export class Tier implements OnInit{
   onBack() {
     this.router.navigate([`/${this.activeOcean}`])
   }
+
+  selectItem(id: number): void {
+    this.selectedItemId = id;
+  }
+
+  scroll(direction: 'left' | 'right'){
+    const container = this.scrollContainer.nativeElement
+    const scrollAmount = 200
+
+    if(direction === 'left') {
+      container.scrollLeft -= scrollAmount;
+    } else {
+      container.scrollLeft += scrollAmount;
+    }
+  }
+
+  items: TierDto[] = [
+    { id: 1, src: 'https://via.placeholder.com/250', alt: 'Tier 1' },
+    { id: 2, src: 'https://via.placeholder.com/250', alt: 'Tier 2' },
+    { id: 3, src: 'https://via.placeholder.com/250', alt: 'Tier 3' },
+    { id: 4, src: 'https://via.placeholder.com/250', alt: 'Tier 4' },
+    { id: 5, src: 'https://via.placeholder.com/250', alt: 'Tier 5' },
+    { id: 6, src: 'https://via.placeholder.com/250', alt: 'Tier 6' },
+    { id: 7, src: 'https://via.placeholder.com/250', alt: 'Tier 7' },
+    { id: 8, src: 'https://via.placeholder.com/250', alt: 'Tier 8' },
+  ];
+
 
 }
