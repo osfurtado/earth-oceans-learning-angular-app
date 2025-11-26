8e5fe19f0a29857a33b1485883fe342d58b466b2
Osvaldo Furtado <osvaldofurtado.bitlc@gmail.com>
2025-11-26 11:26:18 +0100

    Still working on Quiz Page

--- src/app/quiz/quiz.ts
+++ src/app/quiz/quiz.ts
@@ -10,6 +10,16 @@ import { MatAnchor } from "@angular/material/button";
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 
+
+
+
+interface QuizBeantwortung extends Frage {
+  beantwortet: boolean;
+  richtig: boolean | null;
+  selectedOptionId: number | null;
+}
+
+
 @Component({
   selector: 'app-quiz',
   imports: [AsyncPipe, MatAnchor, MatButtonToggleModule, MatIconModule],
@@ -19,22 +29,27 @@ import { MatIconModule } from '@angular/material/icon';
 export class Quiz implements OnInit{
 
   route = inject(ActivatedRoute)
+
+  //Ozean Tier Quiz Daten
   activeOcean: string| null = null 
   activeOzeanId: number = 0
   activeTierId: number = 0
   selectedTier!: TierDto
   selectedMeer!: MeerDto
   quiz!: QuizDto
-  frage!: Frage[]
+  meerObservable!: Observable<MeerDto>
+  meerService = inject(MeerService)
+  source: string = ''
+  
 
   // Quiz steuern
   geantwortet = false
   selectedOptionId: number | null = null
+  quizBeantwortung: QuizBeantwortung[] = []
+  activeFrage!: QuizBeantwortung
 
 
-  meerObservable!: Observable<MeerDto>
-  meerService = inject(MeerService)
-  source: string = ''
+
   
 
   async ngOnInit() {
@@ -44,7 +59,7 @@ export class Quiz implements OnInit{
 
       if(this.source == 'tier'){      
         this.activeTierId =  params['tierId']
-        console.log('Source: Tier ', this.activeTierId)
+
       }     
       
     })
@@ -52,28 +67,63 @@ export class Quiz implements OnInit{
     //Meer
     this.meerObservable = this.meerService.getMeerById(this.activeOzeanId);
     this.selectedMeer = await lastValueFrom(this.meerObservable)
-    console.log('Selected Meer: ', this.selectedMeer.name)
+
+    
  
     //Tier
     if(this.source == 'tier'){
       this.selectedTier = this.selectedMeer.tiere.filter( t => t.id == this.activeTierId)[0]
-      console.log('Nome do animal: ',this.selectedTier.name)
+
     }
 
     //Quiz
     this.quiz = this.selectedMeer.quiz[0]
-    this.frage = this.quiz.frage
+    
+    this.quiz.frage.forEach( f => {
+
+
+      this.quizBeantwortung.push(
+        {
+          id: f.id,
+          frageText: f.frageText,
+          optionen: f.optionen,
+          beantwortet: false,
+          richtig: null,
+          selectedOptionId: null
+        }
+      )
+    })
+
+    
+    this.activeFrage = this.quizBeantwortung[0]
+    console.log('Quiz Active Frage: ',this.activeFrage )
 
 
   }
 
   onOptionClick(op: Option){
-    if(this.geantwortet){return}
+    if(this.activeFrage.beantwortet){return}
+      this.activeFrage.beantwortet = true
+      this.activeFrage.selectedOptionId = op.id
+      this.quizBeantwortung.filter(f => f.id === this.activeFrage.id)[0].beantwortet = true
+      this.quizBeantwortung.filter(f => f.id === this.activeFrage.id)[0].selectedOptionId = op.id
 
-    this.geantwortet = true
+      if(op.istRichtig){
+        this.activeFrage.richtig = true
+        this.quizBeantwortung.filter(f => f.id === this.activeFrage.id)[0].richtig = true
+      } else {
+        this.activeFrage.richtig = false
+        this.quizBeantwortung.filter(f => f.id === this.activeFrage.id)[0].richtig = false
+      }
 
-    this.selectedOptionId = op.id
+      console.log('Quiz Active Frage: ',this.activeFrage )
+      console.log('Quiz Beantwortung: ',this.quizBeantwortung )
 
+  }
+
+  onFrageNummerClick(frageId: number){
+    this.activeFrage = this.quizBeantwortung.filter(f => f.id === frageId)[0]
+    console.log('Quiz Active Frage: ',this.activeFrage )
   }
 
 
